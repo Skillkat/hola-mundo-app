a75fdd1b1e0621bc43ebdcb962f6c75ea09c50c3
Skillkat <66493830+Skillkat@users.noreply.github.com>
2025-08-24 19:52:09 -0400

    prueba 4

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hola Mundooooo!');
+  res.send('Hola Mundoooo!');
 });
 
 app.listen(port, () => {
