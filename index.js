4cb96ffdb86f4bcb690cc814523b769efec5aed3
Skillkat <66493830+Skillkat@users.noreply.github.com>
2025-08-24 20:27:11 -0400

    DEPLOYYYY

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hola Mundoo!');
+  res.send('Hola Mundo!');
 });
 
 app.listen(port, () => {
